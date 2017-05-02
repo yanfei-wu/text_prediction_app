dad45e6c59ad3f79f3a87b17bdbb120af4e976cc
yanfei wu <wuxxx728@umn.edu>
2017-05-02 13:06:51 -0700

    minor fix

--- 1. Data/GetData.R
+++ 1. Data/GetData.R
@@ -1,29 +1,54 @@
-## Get data from Locale en_US
 
+########################## twitters exploration #################################
 # Twitter data (en_US.twitter.txt)
 ustwitter <- read.table('en_US.twitter.txt', sep = '\t', stringsAsFactors = F)
 head(ustwitter, 3)
 str(ustwitter)
 
 # There are some rows containing more than 1 twits as they are newline seperated. 
-# We need to further break them down to single twit.
-s <- s <- strsplit(ustwitter$V1, '\n')
+# need to further break them down to single twit.
+s <- strsplit(ustwitter$V1, '\n')
 ustwitter_df <- data.frame(V1 = unlist(s), stringsAsFactors = F)
 str(ustwitter_df)  
 
-# Find twits containing certain words/sentences.
+# explore regular expression with text data 
+# find twits containing 'biostats'
 grep('biostats', dd$V1, perl = T, value = T) 
-# show twits with 'biostats' in them
-length(grep('love', dd$V1, perl = T, value = F)) 
+
 # show the number of twits with 'love' in them
-length(grep('hate', dd$V1, perl = T, value = F)) 
+length(grep('love', dd$V1, perl = T, value = F)) 
+
 # show the number of twits with 'hate' in them
+length(grep('hate', dd$V1, perl = T, value = F)) 
+
+# show the number of twits with a particular sentence 
 length(grep('A computer once beat me at chess, but it was no match for me at kickboxing', 
             dd$V1, perl = T, value = F))
-# show the number of twits with a sentence 'A computer once beat me at chess, 
-# but it was no match for me at kickboxing' in it.
 
-# Find the longest twits length
+# find the longest twits length
 ustwitter_df['Len'] <- nchar(ustwitter_df$V1)
 head(sort(ustwitter_df$V1, decreasing = T), 1)
+
+
+########################## get blogs and news data ############################# 
+# read blogs and news data
+blog <- readLines('en_US.blogs.txt', encoding = 'UTF-8', skipNul = T)
+news <- readLines('en_US.news.txt', encoding = 'UTF-8', skipNul = T)
+
+
+########################## new corpus with sample data ######################### 
+# randomly sample 5% of data from each of the 3 data sources
+set.seed(25) # set a seed
+sampletwitter <- twitter[sample(1:twitter_lines, twitter_lines*0.05)]
+sampleblog <- blog[sample(1:blog_lines, blog_lines*0.05)]
+samplenews <- news[sample(1:news_lines, news_lines*0.05)]
+writeLines(c(sampletwitter, sampleblog, samplenews), 'sample.txt')
+rm(twitter, blog, news, sampletwitter, sampleblog, samplenews) # free up memory
+
+# construct a new corpus with the sample data
+sample <- readLines('sample.txt', encoding = 'UTF-8', skipNul = T)
+sample <- tolower(sample)
+corpus <- corpus(sample)
+
+
 
